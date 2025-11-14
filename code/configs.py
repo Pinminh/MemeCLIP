eaf4027334ab159b44ee61a1a1ae38ab5903cb89
Pinminh <135088449+Pinminh@users.noreply.github.com>
2025-11-15 04:01:51 +0700

    Adapt configs for ViHarMe usage

--- code/configs.py
+++ code/configs.py
@@ -2,29 +2,35 @@ import os
 from yacs.config import CfgNode 
 
 cfg = CfgNode()
-cfg.root_dir = ''
-cfg.img_folder = ''
-cfg.info_file = ''
+cfg.root_dir = '/content/drive/MyDrive/ViHarMe/sample-data'
+cfg.img_folder = '/content/drive/MyDrive/ViHarMe/sample-data/images'
+cfg.info_file = '/content/drive/MyDrive/ViHarMe/sample-data/memeclip.csv'
 cfg.checkpoint_path = os.path.join(cfg.root_dir, 'checkpoints')
 cfg.checkpoint_file = os.path.join(cfg.checkpoint_path,'model.ckpt')
 
 cfg.clip_variant = "ViT-L/14"
 cfg.dataset_name = 'Pride'
 cfg.name = 'MemeCLIP' 
-cfg.label = 'hate'
+cfg.label = 'label'
 cfg.seed = 42
 cfg.test_only = False
+cfg.reproduce = False
 cfg.device = 'cuda'
 cfg.gpus = [0]
 
-if cfg.label =='hate':
-    cfg.class_names = ['Benign Meme', 'Harmful Meme']
-elif cfg.label == 'humour':
-    cfg.class_names = ['No Humour', 'Humour']
+# if cfg.label =='hate':
+#     cfg.class_names = ['Benign Meme', 'Harmful Meme']
+# elif cfg.label == 'humour':
+#     cfg.class_names = ['No Humour', 'Humour']
+# elif cfg.label == 'target':
+#     cfg.class_names = ['No particular target', 'Individual', 'Community', 'Organization']
+# elif cfg.label == 'stance':
+#     cfg.class_names = ['Neutral', 'Support', 'Oppose']
+
+if cfg.label == 'label':
+    cfg.class_names = ['harmless', 'harmful']
 elif cfg.label == 'target':
-    cfg.class_names = ['No particular target', 'Individual', 'Community', 'Organization']
-elif cfg.label == 'stance':
-    cfg.class_names = ['Neutral', 'Support', 'Oppose']
+    cfg.class_names = ['individual', 'organization', 'community', 'local', 'society']
   
 cfg.batch_size = 16
 cfg.image_size = 224
